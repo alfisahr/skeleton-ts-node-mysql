f50aacce675ef9f2f9635e136973f06f78addad2
alfisahr <alfin.andri@gmail.com>
2023-07-21 09:55:26 +0700

    Adding 404 response

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,12 +2,9 @@ import { Request, Response, Router } from 'express';
 import { postController } from '../controllers/PostController';
 import { userController } from '../controllers/UserController';
 import { categoryController } from '../controllers/CategoryController';
+import AppError from '../middleware/appError';
 
 const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-   res.send(`<h1>Wahai Universe</h1>`);
-});
 
 router.get('/posts', postController.getAll);
 router.post('/posts', postController.create);
@@ -24,5 +21,13 @@ router
    .put('/category', categoryController.update)
    .delete('/category', categoryController.remove)
    .get('/category', categoryController.getAll);
+
+router.route('/').get((req, res) => {
+   res.status(200).send('<h1>Hello universe</h1>');
+});
+
+router.route('*').all((req, res, next) => {
+   next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
+});
 
 export default router;
